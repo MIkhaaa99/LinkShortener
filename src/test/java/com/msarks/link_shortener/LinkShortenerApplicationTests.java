9a7feb8ea56837c56855211b92f999b8f52cb5f1
mikhail <m.s1999@mail.ru>
2024-11-12 20:17:49 +0300

    First commit

---
+++ src/test/java/com/msarks/link_shortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msarks.link_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
